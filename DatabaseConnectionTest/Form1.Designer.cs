d15bb812f65d47467068e07df5652f19df250fd7
IslamMarouf <islammarouf2012@gmail.com>
2019-02-07 10:09:02 +0200

    updated functionality

--- DatabaseConnectionTest/Form1.Designer.cs
+++ DatabaseConnectionTest/Form1.Designer.cs
@@ -28,21 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(13, 13);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(100, 20);
+            this.textEdit1.TabIndex = 0;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(13, 40);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(100, 23);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "simpleButton1";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(13, 70);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(100, 23);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "simpleButton2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(477, 277);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.textEdit1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
     }
 }
 
--- DatabaseConnectionTest/Form1.cs
+++ DatabaseConnectionTest/Form1.cs
@@ -8,6 +8,7 @@ using DatabaseConnectionTest.Properties;
 namespace DatabaseConnectionTest
 {
     public partial class Form1 : Form {
+        public string subjectsDbPath;
         public readonly OleDbConnection SubjectsConnection = new OleDbConnection();
 
         public Form1()
@@ -16,28 +17,14 @@ namespace DatabaseConnectionTest
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            string subjectsDbPath = Settings.Default.subjectsDbPath;
+            subjectsDbPath = Settings.Default.subjectsDbPath;
             subjectsDbPath = "";
-            DialogResult dlgresult;
-            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            
 
             
 
             if (subjectsDbPath.Equals("")) {
-                // Getting installation folder from user
-                folderDlg.Description = "برجاء اختيار مجلد لحفظ قاعدة البيانات";
-                folderDlg.RootFolder = Environment.SpecialFolder.MyComputer;
-
-                dlgresult = folderDlg.ShowDialog();
-
-                if (dlgresult == DialogResult.OK) {
-                    // Assigning operators
-                    subjectsDbPath = folderDlg.SelectedPath;
-                    File.WriteAllBytes(subjectsDbPath + @"\Subjects.accdb", Properties.Resources.Subjects);
-                    Settings.Default.subjectsDbPath = subjectsDbPath + @"\";
-                    Settings.Default.Save();
-                    MessageBox.Show(subjectsDbPath);
-                }
+                subjectsDbPath = GetDatabasePath();
             }
             else {
                 /*
@@ -70,13 +57,46 @@ namespace DatabaseConnectionTest
             }
             catch (InvalidOperationException invOpEx)
             {
-                MessageBox.Show("Invalid Message = " + invOpEx.Message);
+                DialogResult =
+                MessageBox.Show("", "تعذر الوصول الى قاعدة البيانات", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+
+                switch (DialogResult) {
+                    case DialogResult.Retry:
+                        subjectsDbPath = GetDatabasePath();
+                        break;
+                    case DialogResult.Cancel:
+                        MessageBox.Show("Invalid Message = " +
+                                        "\n\nError Message = " + invOpEx.Message);
+                        break;
+                }       
             }
 
-            if (SubjectsConnection.State != ConnectionState.Open)
-            {
-                MessageBox.Show("Subjects Database connection is failed.");
+            if (SubjectsConnection.State != ConnectionState.Open) {
+                simpleButton1.Enabled = false;
+                simpleButton2.Enabled = false;
+                textEdit1.Enabled = false;}
+        }
+
+        private string GetDatabasePath() {
+            string subjectsDbPath = string.Empty;DialogResult dlgresult;
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            // Getting installation folder from user
+            folderDlg.Description = "برجاء اختيار مجلد لحفظ قاعدة البيانات";
+            folderDlg.RootFolder = Environment.SpecialFolder.MyComputer;
+
+            dlgresult = folderDlg.ShowDialog();
+
+            if (dlgresult == DialogResult.OK) {
+                // Assigning operators
+                subjectsDbPath = folderDlg.SelectedPath;
+                File.WriteAllBytes(subjectsDbPath + @"\Subjects.accdb", Properties.Resources.Subjects);
+                Settings.Default.subjectsDbPath = subjectsDbPath + @"\";
+                Settings.Default.Save();
+                MessageBox.Show(subjectsDbPath);
             }
+
+            return subjectsDbPath;
         }
     }
 }
